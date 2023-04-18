c969c0d4a810e9cda20d5922063d02b9302b2a36
Joel Kwihangana <joelkwijoe@gmail.com>
2023-04-18 05:04:37 +0200

    adding styles to components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div>
-      <h2>Getting started with ReactJs</h2>
       <ExpenseItem />
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import "./ExpenseItem.css";
 const ExpenseItem = () => {
-  return <div>ExpenseItem</div>;
+  return (
+    <div className="expense-item">
+      <div className="expense-item__date">April 20th 2023</div>
+      <div className="expense-item__description">
+        <h2>Buying new Laptops</h2>
+      </div>
+      <div className="expense-item__price">$500</div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
