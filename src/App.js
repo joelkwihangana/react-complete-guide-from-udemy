daf91359f0a5b659029337fd82d43bd81861e740
Joel Kwihangana <joelkwijoe@gmail.com>
2023-04-18 03:44:02 +0200

    getting started

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Getting started with ReactJs</h2>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
