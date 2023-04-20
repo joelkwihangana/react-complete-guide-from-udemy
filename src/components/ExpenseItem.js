78906dc668e711399b7b6b485b78eb28f3e3a158
Joel Kwihangana <joelkwijoe@gmail.com>
2023-04-20 16:03:51 +0200

    Outputting Dynamic Data & Working with Expressions in JSX and  Passing Data via _props_

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./ExpenseItem.css";
-const ExpenseItem = () => {
+
+const ExpenseItem = ({ title, amount, date }) => {
   return (
     <div className="expense-item">
-      <div className="expense-item__date">April 20th 2023</div>
+      <div className="expense-item__date">{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Buying new Laptops</h2>
+        <h2>{title}</h2>
       </div>
-      <div className="expense-item__price">$500</div>
+      <div className="expense-item__price">{`$ ${amount}`}</div>
     </div>
   );
 };
