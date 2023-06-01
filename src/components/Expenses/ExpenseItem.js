8bfa8e13ef9d8dfbe1b43401d119c24e4110dc50
Joel Kwihangana <joelkwijoe@gmail.com>
2023-06-01 12:51:57 +0200

    getting started with states, how to use them

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = ({ title, amount, date }) => {
-  const userResponseMessage = (name, message) => {
-    console.log(`Hello ${name} ${message} Thank you!`);
+  const [ourTitle, setourTitle] = useState(title);
+  const changeTitle = () => {
+    let titleText = "Man title irahindutse";
+    setourTitle(titleText);
   };
   return (
     <Card className="expense-item">
@@ -13,19 +15,10 @@ const ExpenseItem = ({ title, amount, date }) => {
         <ExpenseDate date={date} />
       </div>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{ourTitle}</h2>
       </div>
       <div className="expense-item__price">{`$ ${amount}`}</div>
-      <button
-        onClick={() => {
-          userResponseMessage(
-            "Joel KWIHANGANA",
-            "iyi ni message iza iyo ukanze ibuto!"
-          );
-        }}
-      >
-        Change title
-      </button>
+      <button onClick={changeTitle}>Change title</button>
     </Card>
   );
 };
