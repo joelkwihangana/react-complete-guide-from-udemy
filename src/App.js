57d118cd872fbbd6997aff38ddd67ab5c2f131df
Joel Kwihangana <joelkwijoe@gmail.com>
2023-06-22 06:08:19 +0200

    add new expense to the list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-// import ExpenseItem from "./components/Expenses/ExpenseItem";
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
-  const expenses = [
+  const DUMMY_EXPENSES = [
     {
       id: "e1",
       title: "Toilet Paper",
@@ -24,9 +24,10 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In app.js", expense);
+    setExpenses([expense, ...expenses]);
   };
   return (
     <div>
