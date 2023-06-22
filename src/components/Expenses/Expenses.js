9e26ea29cd4d47a5426ec3ed5b2d089572fac188
Joel Kwihangana <joelkwijoe@gmail.com>
2023-06-22 05:55:37 +0200

    using map array method to render item list dynamically

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,26 +8,9 @@ const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpensesFilter />
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title="Total Expanses"
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
+      {props.expenses.map((item) => (
+        <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+      ))}
     </Card>
   );
 };
